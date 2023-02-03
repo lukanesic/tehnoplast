37052285e3a9fb14510dc54e7eacf62c4d224f05
Luka <pablolucasso@icloud.com>
2023-02-03 12:14:39 +0100

    N

--- components/VideoSection/index.js
+++ components/VideoSection/index.js
@@ -3,7 +3,14 @@ import React from 'react'
 const VideoSection = () => {
   return (
     <div className='video-section'>
-      <video src={'images/video.mp4'} type='video/mp4' loop muted autoPlay />
+      <video
+        src={'images/video.mp4'}
+        type='video/mp4'
+        loop
+        muted
+        autoPlay
+        playsInline
+      />
     </div>
   )
 }
