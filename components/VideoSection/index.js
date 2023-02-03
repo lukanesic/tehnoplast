851d483a7d98cdeeff7e45dcebd044ff8eaf477e
Luka <pablolucasso@icloud.com>
2023-02-03 12:13:28 +0100

    New error fix

--- components/VideoSection/index.js
+++ components/VideoSection/index.js
@@ -3,14 +3,7 @@ import React from 'react'
 const VideoSection = () => {
   return (
     <div className='video-section'>
-      <video
-        src={'images/video.mp4'}
-        type='video/mp4'
-        loop
-        muted
-        autoplay
-        playsinline
-      />
+      <video src={'images/video.mp4'} type='video/mp4' loop muted autoPlay />
     </div>
   )
 }
