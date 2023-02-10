6456065b75a9716587a1de02628aeb7b70602255
Luka <pablolucasso@icloud.com>
2023-02-10 08:43:00 +0100

    Product page #2

--- components/Product/index.js
+++ components/Product/index.js
@@ -11,7 +11,6 @@ const Product = ({ data }) => {
       <ProductTopSection data={data[0]} />
       <ProductMidSection data={data[0]} />
       <ProductBotSection data={data[0]} cls={'bot-section'} />
-      <ProductBotSection data={data[0]} cls={'bot-section reverse'} />
     </div>
   )
 }
@@ -19,8 +18,6 @@ const Product = ({ data }) => {
 export default Product
 
 const ProductTopSection = ({ data }) => {
-  console.log(data)
-
   return (
     <div className='p-top-section'>
       <div className='profil-img'>
@@ -46,56 +43,126 @@ const ProductTopSection = ({ data }) => {
   )
 }
 
-const ProductMidSection = () => {
+const ProductMidSection = ({ data }) => {
   return (
     <div className='mid-section'>
       <div className='box'>
-        <h6>Svojstva</h6>
+        <h6>{data && data.properties.title}</h6>
+        <ul>
+          <li>
+            {data && data.properties.prop1Title}{' '}
+            <span>{data && data.properties.prop1}</span>
+          </li>
+
+          <li>
+            {data && data.properties.prop2Title}{' '}
+            <span>{data && data.properties.prop2}</span>
+          </li>
+
+          <li>
+            {data && data.properties.prop3Title}{' '}
+            <span>{data && data.properties.prop3}</span>
+          </li>
+
+          <li>
+            {data && data.properties.prop4Title}{' '}
+            <span>{data && data.properties.prop4}</span>
+          </li>
+        </ul>
       </div>
       <div className='box'>
-        <h6>Dizajn</h6>
-        <span>
-          <a href='#' className='mail'>
-            info@tehnoplast.co.rs
-          </a>
-        </span>
+        <h6>{data && data.design.title}</h6>
+        <ul>
+          <li>
+            {data && data.design.prop1Title}{' '}
+            <span>{data && data.design.prop1}</span>
+          </li>
+
+          <li>
+            {data && data.design.prop2Title}{' '}
+            <span>{data && data.design.prop2}</span>
+          </li>
+
+          <li>
+            {data && data.design.prop3Title}{' '}
+            <span>{data && data.design.prop3}</span>
+          </li>
+
+          <li>
+            {data && data.design.prop4Title}{' '}
+            <span>{data && data.design.prop4}</span>
+          </li>
+        </ul>
       </div>
       <div className='box'>
-        <h6>Performans</h6>
-        <span>
-          <a href='#'>+ 38163601283, Ivank</a>
-        </span>
-        <span>
-          <a href='#'>+ 38163601283, Stefan</a>
-        </span>
-        <span>
-          <a href='#'>+ 38134852123, Tel/Fax</a>
-        </span>
+        <h6>{data && data.performans.title}</h6>
+        <ul>
+          <li>
+            {data && data.performans.prop1Title}{' '}
+            <span>{data && data.performans.prop1}</span>
+          </li>
+
+          <li>
+            {data && data.performans.prop2Title}{' '}
+            <span>{data && data.performans.prop2}</span>
+          </li>
+
+          <li>
+            {data && data.performans.prop3Title}{' '}
+            <span>{data && data.performans.prop3}</span>
+          </li>
+
+          <li>
+            {data && data.performans.prop4Title}{' '}
+            <span>{data && data.performans.prop4}</span>
+          </li>
+
+          <li>
+            {data && data.performans.prop5Title}{' '}
+            <span>{data && data.performans.prop5}</span>
+          </li>
+        </ul>
       </div>
     </div>
   )
 }
 
-const ProductBotSection = ({ cls }) => {
+const ProductBotSection = ({ cls, data }) => {
   return (
-    <div className={cls}>
-      <div className='profil-bot-img'>
-        <Image src={thermo} alt={`Test Tehnoplast`} fill objectFit='cover' />
-      </div>
-      <div className='profil-info'>
-        <h3>Energetski nivoi</h3>
+    <>
+      <div className={cls}>
+        <div className='profil-bot-img'>
+          <Image
+            src={data && data.thermo}
+            alt={data && data.subtitle1}
+            fill
+            objectFit='cover'
+          />
+        </div>
+        <div className='profil-info'>
+          <h3>{data && data.subtitle1}</h3>
 
-        <p>
-          Učinili smo ga neuporedivim po energetskim performansama,
-          neprikosnovenim i nepropusnim. Napravili smo ga kompletnim i
-          raznovrsnim tako da možete da ga učinite divnim.
-        </p>
-        <p>
-          Performanse ICONIC V77 AI² na svim faktorima koji utiču na unutrašnje
-          okruženje su izvanredne i sertifikovane od strane Instituta Ift
-          Rosenheim u Nemačkoj.
-        </p>
+          <p>{data && data.subtitle1Desc1}</p>
+          <p>{data && data.subtitle1Desc2}</p>
+        </div>
       </div>
-    </div>
+
+      <div className={`${cls} reverse`}>
+        <div className='profil-bot-img'>
+          <Image
+            src={data && data.duration}
+            alt={data && data.subtitle2}
+            fill
+            objectFit='cover'
+          />
+        </div>
+        <div className='profil-info'>
+          <h3>{data && data.subtitle2}</h3>
+
+          <p>{data && data.subtitle2Desc1}</p>
+          <p>{data && data.subtitle2Desc2}</p>
+        </div>
+      </div>
+    </>
   )
 }
